7bd57dd8683ca9702bb1b79e44c9ae1392465358
Kenneth Ossai <kenossai1@gmail.com>
2026-03-31 17:19:39 +0100

    Update MinistryForm.php

--- app/Filament/Resources/Ministries/Schemas/MinistryForm.php
+++ app/Filament/Resources/Ministries/Schemas/MinistryForm.php
@@ -13,6 +13,7 @@ class MinistryForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(1)
             ->components([
                 Section::make('Ministry Details')
                     ->columns(2)
